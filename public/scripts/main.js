c60524202ad5262a6955266cce5416870df77b50
Manon Rugge <manonrugge@gmail.com>
2018-08-27 18:01:17 +0200

    jQuery functionality working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,21 @@
-"use strict";
+'use strict';
 
-var ohHey = "Hello World";
+jQuery(document).ready(function () {
+    // $('.menu-item').addClass('menu-trigger');
 
-console.log(ohHey);
+    $('.hamburger').click(function () {
+        // console.log('hello');
+        // $('#hamburger').toggleClass('clicked');
+        $('.header-wrapper').toggleClass('push');
+        $('.side-nav-inner').toggleClass('open');
+    });
+
+    $('.site-nav-left').hover(function () {
+        // console.log('hello');
+        $('.arrow-left').toggleClass('move-left');
+    });
+
+    $('.site-nav-right').hover(function () {
+        $('.arrow-right').toggleClass('move-right');
+    });
+});
